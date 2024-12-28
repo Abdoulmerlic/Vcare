2df6c12c2e9fdf4e7274df15c1a6d13938891dd5
Abdoulmerlic <abdulmaliksaidu289@gmail.com>
2024-12-28 20:19:08 +0100

    h

---
+++ doctor-dashboard/js/chat-patient.js
@@ -0,0 +1,14 @@
+document.getElementById('profilePicture').addEventListener('click', function() {
+    document.getElementById('file-upload').click();
+});
+
+document.getElementById('file-upload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profileImage').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+}); 
